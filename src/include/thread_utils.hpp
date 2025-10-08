f329fe4c6390f02a7f45b2854494989705af71c5
dentiny <dentinyhao@gmail.com>
2025-10-08 04:59:38 +0000

    Add benchmark

---
+++ src/include/thread_utils.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <string>
+
+namespace duckdb {
+
+// Get the number of cores available to the system.
+// On linux platform, this function not only gets physical core number for CPU, but also considers available core number
+// within kubernetes pod, and container cgroup resource.
+int GetCpuCoreCount();
+
+} // namespace duckdb
